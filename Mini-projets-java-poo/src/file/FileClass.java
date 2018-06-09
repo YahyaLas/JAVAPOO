70d9222224cfde687c1917e29a0be910b829e547
yahya.lassiri.96@gmail.com <yahya.lassiri.96@gmail.com>
2018-06-09 13:50:00 +0200

    # Ajout des méthode qui permet d'afficher le contenu du répertoire et les sous répertoire

--- Mini-projets-java-poo/src/file/FileClass.java
+++ Mini-projets-java-poo/src/file/FileClass.java
@@ -1,9 +1,14 @@
 package file;
 
 import java.io.File;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+import constants.ConstantMsgClass;
+import saisie.SaisieClass;
 
 public class FileClass {
+
 	/**
 	 * Affiche le contenu d’un répertoire en affichant si les éléments de ce
 	 * répertoire sont des fichier ou des répertoires. Dans le cas ou il s’agit d’un
@@ -12,8 +17,7 @@ public class FileClass {
 	 * @param rep
 	 *            emplacement répértoire
 	 */
-	public void afficheContenu(String rep) {
-
+	public static void afficheContenu(String rep) {
 		File f = new File(rep);
 		if (f.exists()) {
 			String[] contenu = f.list();
@@ -37,12 +41,62 @@ public class FileClass {
 	 * @param rep
 	 *            emplacement répértoire
 	 */
-	public void afficheContenu() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Veuillez saisir l'emplacement du répértoire:");
-		String src = sc.nextLine();
-		sc.close();
+	public static void afficheContenu() {
+		String src = SaisieClass.saisieStringConsole(ConstantMsgClass.FILECLASS_SAISIE_MSG_SAISIE_REPERTOIRE);
 		afficheContenu(src);
+	}
+
+	/**
+	 * Permet d’afficher le contenu d’un répertoire y compris le contenu de ses sous
+	 * répertoires
+	 */
+	public static void afficheContenuComprisSousReprtoires() {
+		String src = SaisieClass.saisieStringConsole(ConstantMsgClass.FILECLASS_SAISIE_MSG_SAISIE_REPERTOIRE);
+		afficheContenuComprisSousReprtoires("", src);
+	}
+
+	/**
+	 * Permet d’afficher le contenu d’un répertoire y compris le contenu de ses sous
+	 * répertoires
+	 * 
+	 * @param rep
+	 *            emplacement répértoire
+	 */
+	public static void afficheContenuComprisSousReprtoires(String rep) {
+		afficheContenuComprisSousReprtoires("", rep);
+	}
+
+	/**
+	 * Permet d’afficher le contenu d’un répertoire y compris le contenu de ses sous
+	 * répertoires
+	 * 
+	 * @param rep
+	 *            emplacement répértoire
+	 * @param espace
+	 */
+	private static void afficheContenuComprisSousReprtoires(String espace, String rep) {
+
+		File f = new File(rep);
+		if (f.exists()) {
+			String[] contenu = f.list();
+			if (contenu == null) 
+				return;
+			List<String> listDirectory = new ArrayList<String>();
+			for (int i = 0; i < contenu.length; i++) {
+				File f2 = new File(rep, contenu[i]);
+				if (f2.isDirectory()) {
+					listDirectory.add(contenu[i]);
+				} else
+					System.out.println(espace + "Fichier : " + contenu[i] + "Size : " + contenu[i].length());
+			}
+			for (String elem : listDirectory) {
+				System.out.println(espace + "REP : " + elem);
+				afficheContenuComprisSousReprtoires(espace + "\t", rep + "/" + elem);
+			}
+		} else {
+			System.out.println(rep + " n'existe pas");
+		}
+
 	}
 
 }
