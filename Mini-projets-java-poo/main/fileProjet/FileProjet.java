98cc35659399911b77ac114277e4d5b6cd662fac
yahya.lassiri.96@gmail.com <yahya.lassiri.96@gmail.com>
2018-06-09 13:51:18 +0200

    # Ajout Affichage du contenu d’un répertoire y compris le contenu de ses sous répertoires

--- Mini-projets-java-poo/main/fileProjet/FileProjet.java
+++ Mini-projets-java-poo/main/fileProjet/FileProjet.java
@@ -1,15 +1,29 @@
 package fileProjet;
 
-
 import file.FileClass;
 
 public class FileProjet {
 
 	public static void main(String[] args) {
-		
-		FileClass fileClass = new FileClass();
-		fileClass.afficheContenu("c:/");
-		fileClass.afficheContenu();
+		/*
+		 * Afficher le contenu d’un répertoire saisie en affichant si les éléments de ce
+		 * répertoire sont des fichier ou des répertoires. Dans le cas ou il s’agit d’un
+		 * fichier elle affiche la capacité physique du fichier.
+		 */
+		System.out.println("\r\n**Afficher le contenu d’un répertoire saisie en affichant si les éléments de ce"
+				+ " répertoire sont des fichier ou des répertoires. Dans le cas ou il s’agit d’un"
+				+ "fichier elle affiche la capacité physique du fichier.");
+		FileClass.afficheContenu("C:/");
+		FileClass.afficheContenu();
+
+		/*
+		 * afficher le contenu d’un répertoire y compris le contenu de ses sous
+		 * répertoires
+		 */
+		System.out.println(
+				"\r\n**Afficher le contenu d’un répertoire y compris le contenu de ses sous répertoires\r\n**");
+		FileClass.afficheContenuComprisSousReprtoires("C:/Users/");
+		FileClass.afficheContenuComprisSousReprtoires();
 	}
 
 }
