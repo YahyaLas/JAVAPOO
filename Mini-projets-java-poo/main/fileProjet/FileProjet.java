e620e060008a85afc15fc37c6e26f54953f637eb
yahya.lassiri.96@gmail.com <yahya.lassiri.96@gmail.com>
2018-06-09 16:09:59 +0200

    # Ajout des méthodes pour crypter et décrypter un fichier txt

--- Mini-projets-java-poo/main/fileProjet/FileProjet.java
+++ Mini-projets-java-poo/main/fileProjet/FileProjet.java
@@ -12,27 +12,44 @@ public class FileProjet {
 		 * répertoire sont des fichier ou des répertoires. Dans le cas ou il s’agit d’un
 		 * fichier elle affiche la capacité physique du fichier.
 		 */
-//		System.out.println("\r\n**Afficher le contenu d’un répertoire saisie en affichant si les éléments de ce"
-//				+ " répertoire sont des fichier ou des répertoires. Dans le cas ou il s’agit d’un"
-//				+ "fichier elle affiche la capacité physique du fichier.");
-//		FileClass.afficheContenu("C:/");
-//		FileClass.afficheContenu();
+		// System.out.println("\r\n**Afficher le contenu d’un répertoire saisie en
+		// affichant si les éléments de ce"
+		// + " répertoire sont des fichier ou des répertoires. Dans le cas ou il s’agit
+		// d’un"
+		// + "fichier elle affiche la capacité physique du fichier.");
+		// FileClass.afficheContenu("C:/");
+		// FileClass.afficheContenu();
 
 		/*
 		 * afficher le contenu d’un répertoire y compris le contenu de ses sous
 		 * répertoires
 		 */
-//		System.out.println(
-//				"\r\n**Afficher le contenu d’un répertoire y compris le contenu de ses sous répertoires\r\n**");
-//		FileClass.afficheContenuComprisSousReprtoires("C:/Users/");
-//		FileClass.afficheContenuComprisSousReprtoires();
+		// System.out.println(
+		// "\r\n**Afficher le contenu d’un répertoire y compris le contenu de ses sous
+		// répertoires\r\n**");
+		// FileClass.afficheContenuComprisSousReprtoires("C:/Users/");
+		// FileClass.afficheContenuComprisSousReprtoires();
 
 		/*
 		 * Copier Contenu d'un Fichier text dans un autre
 		 */
+//		System.out.println("\r\n**Copier Contenu d'un fichier text dans un autre**\r\n");
+//		FileClass.copierFichierTextDansUnAutre("ressources/File1.txt", "ressources/File2");
+		
+		/*
+		 * Copier Contenu d'un Fichier text dans un autre avec cryptage personnalisé
+		 */
+//		System.out.println(
+//				"\r\n**Copier Contenu d'un fichier text dans un autre avec cryptage**\r\n");
+//		FileClass.copierFichierTextDansUnAutreAvecCryptage("ressources/File1.txt","ressources/File2.txt");
+	
+		/*
+		 * décrypté un fichier qui est crypté par la fonction de cryptage du FileClass
+		 */
 		System.out.println(
-				"\r\n**Copier Contenu d'un fichier text dans un autre**\r\n");
-		FileClass.copierFichierTextDansUnAutre("ressources/File1.txt","ressources/File2");
+				"\r\n**Décrypter un fichier qu'on a crypté avec notre cryptage**\r\n");
+		FileClass.decrypterFichier("ressources/File2.txt");
+
 	}
 
 }
