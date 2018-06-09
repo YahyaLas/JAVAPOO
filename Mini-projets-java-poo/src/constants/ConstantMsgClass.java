337d0a8ad237eece86be3e67e06f76c8ff25aecc
yahya.lassiri.96@gmail.com <yahya.lassiri.96@gmail.com>
2018-06-09 13:47:05 +0200

    #Ajout d'une classe de constante message

---
+++ Mini-projets-java-poo/src/constants/ConstantMsgClass.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class ConstantMsgClass {
+	/****************** Scanner ***********************/
+	/****************** FileClass ***********************/
+	public final static String FILECLASS_SAISIE_MSG_SAISIE_REPERTOIRE = "Veuillez saisir l'emplacement du répértoire: ";
+}
