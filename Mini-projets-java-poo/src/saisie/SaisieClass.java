195fb0c028955b1f2e097d87e926ca82a5af1167
yahya.lassiri.96@gmail.com <yahya.lassiri.96@gmail.com>
2018-06-09 13:48:52 +0200

    #Ajout d'une classe qui contient les différentes méthodes pour le saisie (Actuellement saisie depuis le clavier)

---
+++ Mini-projets-java-poo/src/saisie/SaisieClass.java
@@ -0,0 +1,17 @@
+package saisie;
+
+import java.util.Scanner;
+
+public class SaisieClass {
+	static Scanner sc = new Scanner(System.in);
+	/**
+	 * Retourne un string saisie par l'utilisateur depuis la console
+	 * 
+	 * @return String
+	 */
+	public static String saisieStringConsole(String message) {
+		System.out.println(message);
+		String src = sc.nextLine();
+		return src;
+	}
+}
